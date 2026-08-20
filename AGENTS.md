070585d192ca4ecc2320a423739a2b13e74cb621
PenguinPiplup <127109356+PenguinPiplup@users.noreply.github.com>
2026-08-21 00:27:30 +0800

    Renamed bot and edited welcome message

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Year 2 Computer Science student
+* IDE and level of expertise: Some experience with IntelliJ and building full-stack applications
 
 # Guidance for interacting with users
 
---
+++ src/main/java/PiplupBot.java
@@ -0,0 +1,19 @@
+public class PiplupBot {
+    public static void main(String[] args) {
+        String banner = " ____  _       _             ____        _   \n" +
+                        "|  _ \\(_)_ __ | |_   _ _ __ | __ )  ___ | |_ \n" +
+                        "| |_) | | '_ \\| | | | | '_ \\|  _ \\ / _ \\| __|\n" +
+                        "|  __/| | |_) | | |_| | |_) | |_) | (_) | |_ \n" +
+                        "|_|   |_| .__/|_|\\__,_| .__/|____/ \\___/ \\__|\n" +
+                        "        |_|           |_|                    \n";
+        System.out.println(banner);
+
+        String initialGreetings =
+                "Hello! I'm PiplupBot.\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(initialGreetings);
+    }
+}
