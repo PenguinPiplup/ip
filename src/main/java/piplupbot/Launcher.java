32a0ba1a9d4a7729939734966575c5614464e6fa
PenguinPiplup <127109356+PenguinPiplup@users.noreply.github.com>
2026-09-11 15:40:55 +0800

    Add Gradle Settings

---
+++ src/main/java/piplupbot/Launcher.java
@@ -0,0 +1,12 @@
+package piplupbot;
+
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PiplupBotGui.class, args);
+    }
+}
